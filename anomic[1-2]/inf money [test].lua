fa710a28dcc35b4137f6aafb8676a8661ef2e4d8
vahEzra <lifecycle702@gmail.com>
2024-12-28 13:19:36 -0500

    Update inf money [test].lua

--- anomic[1-2]/inf money [test].lua
+++ anomic[1-2]/inf money [test].lua
@@ -1,8 +1,8 @@
 warn("||| INF Money Script v1 (12/28/24)")
 while wait(00.1) do
-  if game.Players.LocalPlayer.MainUIHolder.MenuBar.CashDisplay then
+  if game.Players.LocalPlayer.MainUIHolder.PlayerGui.MenuBar.CashDisplay then
     print(true)
-    game.Players.LocalPlayer.MainUIHolder.MenuBar.CashDisplay.Text = "999B  $"
+    game.Players.LocalPlayer.PlayerGui.MainUIHolder.MenuBar.CashDisplay.Text = "999B  $"
   else
 print(false)
     print()
@@ -11,4 +11,4 @@ print(false)
     print("Please Get in The Correct Game, or we Can't Detected Anomic!")
   end
 end
-game.Players.LocalPlayer.MainUIHolder.MenuBar.CashDisplay.CashUpdater:Destroy()
+game.Players.LocalPlayer.PlayerGui.MainUIHolder.MenuBar.CashDisplay.CashUpdater:Destroy()
